e2dc73a01b47e20ec8f271d4cb89fa8a0d497034
rafath-bin-zafar-auvee <rafath.auvee@gmail.com>
2023-03-19 22:40:10 +0600

    controller for post user

---
+++ controller/post.controller.js
@@ -0,0 +1,110 @@
+const { Post } = require("../models/blog.model.js");
+
+// Get a list of all posts
+exports.getPosts = async (req, res) => {
+  try {
+    const posts = await Post.find()
+      .populate("author", ["name", "email"]) // populate author field with name and email of the user
+      .sort({ createdAt: -1 }); // sort by creation date in descending order
+    res.json(posts);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+};
+
+// Create a new post
+exports.createPost = async (req, res) => {
+  const { title, content } = req.body;
+
+  try {
+    const post = new Post({
+      title,
+      content,
+      author: req.user.id, // set the author to the currently logged in user
+    });
+
+    await post.save();
+
+    res.json(post);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+};
+
+// Get a single post by ID
+exports.getPostById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const post = await Post.findById(id).populate("author", ["name", "email"]);
+
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+
+    res.json(post);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+};
+
+// Update a post by ID
+exports.updatePost = async (req, res) => {
+  const { id } = req.params;
+  const { title, content } = req.body;
+
+  try {
+    let post = await Post.findById(id);
+
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+
+    // Check if the currently logged in user is the author of the post
+    if (post.author.toString() !== req.user.id) {
+      return res
+        .status(401)
+        .json({ msg: "Not authorized to update this post" });
+    }
+
+    post.title = title || post.title;
+    post.content = content || post.content;
+
+    await post.save();
+
+    res.json(post);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+};
+
+// Delete a post by ID
+exports.deletePost = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    let post = await Post.findById(id);
+
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+
+    // Check if the currently logged in user is the author of the post
+    if (post.author.toString() !== req.user.id) {
+      return res
+        .status(401)
+        .json({ msg: "Not authorized to delete this post" });
+    }
+
+    await post.remove();
+
+    res.json({ msg: "Post removed" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+};
---
+++ controller/user.controller.js
@@ -0,0 +1,97 @@
+const { User } = require("../models/blog.model.js");
+
+// Get a list of all users
+exports.getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+};
+
+// Create a new user
+exports.createUser = async (req, res) => {
+  const { name, email, password, role } = req.body;
+
+  try {
+    let user = new User({
+      name,
+      email,
+      password,
+      role,
+    });
+
+    await user.save();
+
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+};
+
+// Get a single user by ID
+exports.getUserById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const user = await User.findById(id);
+
+    if (!user) {
+      return res.status(404).json({ msg: "User not found" });
+    }
+
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+};
+
+// Update a user by ID
+exports.updateUser = async (req, res) => {
+  const { id } = req.params;
+  const { name, email, password, role } = req.body;
+
+  try {
+    let user = await User.findById(id);
+
+    if (!user) {
+      return res.status(404).json({ msg: "User not found" });
+    }
+
+    user.name = name || user.name;
+    user.email = email || user.email;
+    user.password = password || user.password;
+    user.role = role || user.role;
+
+    await user.save();
+
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+};
+
+// Delete a user by ID
+exports.deleteUser = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    let user = await User.findById(id);
+
+    if (!user) {
+      return res.status(404).json({ msg: "User not found" });
+    }
+
+    await user.remove();
+
+    res.json({ msg: "User removed" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+};
